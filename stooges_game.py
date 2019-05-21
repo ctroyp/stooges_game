6315ae39a71eb5948c678b48efb61bed05fe417b
ctroyp <ctroyp@gmail.com>
2019-05-21 09:05:03 -0400

    Added all stooges and more functions for class

--- stooges_game.py
+++ stooges_game.py
@@ -3,27 +3,44 @@ class GameObject:
     desc = ''
     objects = {}
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, name, desc=''):
+        self.class_name = name
+        self.desc = desc
         GameObject.objects[self.class_name] = self
 
     def get_desc(self):
-        return self.class_name + '\n' + self.desc
+        return f'{self.class_name}\n{self.desc}'
 
 
-class Goblin(GameObject):
-    class_name = 'goblin'
-    desc = 'A foul creature'
+class Stooge(GameObject):
+    def __init__(self, name, desc='Short and just another stooge'):
+        self.class_name = 'stooge'
+        self.health = 3
+        self._desc = desc
+        super().__init__(name, desc)
 
-goblin = Goblin('Gobbly')
+    @property
+    def desc(self):
+        if self.health >= 3:
+            return self._desc
+        elif self.health == 2:
+            health_line = 'He has a wound on its knee.'
+        elif self.health == 1:
+            health_line = 'He is crippled!'
+        elif self.health <= 0:
+            health_line = 'He is no longer with us.'
+        return f'{self._desc}\n{health_line}'
 
+    @desc.setter
+    def desc(self, value):
+        self._desc = value
 
-def examine(noun):
-    if noun in GameObject.objects:
-        return GameObject.objects[noun].get_desc()
-    else:
-        return f'There is no {noun} here.'
-
+moe = Stooge('Moe', 'He\'s the boss-short, bowl-cut hair, and grumpy')
+larry = Stooge('Larry', 'He\'s has an endless supply of hair when '
+                'you pull on it')
+curly = Stooge('Curly', 'He\'s not the smartest stooge on the stage')
+shemp = Stooge('Shemp', 'He\'s the tall goofy stooge that loves '
+                'jumping beans')
 
 def run():
     command = input(' -> ').split()
@@ -38,7 +55,6 @@ def run():
         print(f'Unknown verb {verb_word}')
         return
 
-    print(len(command))
     if len(command) >= 2:
         noun_word = command[1]
         print(verb(noun_word))
@@ -49,14 +65,33 @@ def run():
 def say(noun):
     return f'You said "{noun}".'
 
-
 def poke(noun):
     return f'You poked {noun}!'
 
+def hit(noun):
+    if noun in GameObject.objects:
+        thing = GameObject.objects[noun]
+        if type(thing) == Stooge:
+            thing.health = thing.health - 1
+            if thing.health <= 0:
+                msg = "You may have killed a stooge!"
+            else:
+                msg = f'You hit {thing.class_name}'
+    else:
+        msg = f'There is no {noun} here.'
+    return msg
+
+def examine(noun):
+    if noun in GameObject.objects:
+        return GameObject.objects[noun].get_desc()
+    else:
+        return f'There is no {noun} here.'
 
 verb_dict = {
     'say': say,
-    'poke': poke
+    'poke': poke,
+    'hit': hit,
+    'examine': examine
 }
 
 if __name__ == '__main__':
