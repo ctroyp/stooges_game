07c235eada006598883741a6ff6f5bdf083cabfa
ctroyp <ctroyp@gmail.com>
2019-05-21 07:26:38 -0400

    Added Stooge class

--- stooges_game.py
+++ stooges_game.py
@@ -1,29 +1,64 @@
-def get_input():
-    command = input(": ").split()
-    verb_word = command[0]
+class GameObject:
+    class_name = ''
+    desc = ''
+    objects = {}
+
+    def __init__(self, name):
+        self.name = name
+        GameObject.objects[self.class_name] = self
+
+    def get_desc(self):
+        return self.class_name + '\n' + self.desc
+
+
+class Goblin(GameObject):
+    class_name = 'goblin'
+    desc = 'A foul creature'
+
+goblin = Goblin('Gobbly')
+
+
+def examine(noun):
+    if noun in GameObject.objects:
+        return GameObject.objects[noun].get_desc()
+    else:
+        return f'There is no {noun} here.'
+
+
+def run():
+    command = input(' -> ').split()
+    verb_word = command[0].lower()
+
     if verb_word in verb_dict:
         verb = verb_dict[verb_word]
-    elif verb_word.lower() in {"q", "quit"}:
-        print("Thanks for playing, ya stooge!")
+    elif verb_word.lower() in {'q', 'quit'}:
+        print('Thanks for playing, ya stooge!')
         exit(0)
     else:
-        print("Unknown verb {}". format(verb_word))
+        print(f'Unknown verb {verb_word}')
         return
 
+    print(len(command))
     if len(command) >= 2:
         noun_word = command[1]
         print(verb(noun_word))
     else:
-        print(verb("nothing"))
+        print(verb('nothing'))
 
 
 def say(noun):
-    return 'You said "{}"'.format(noun)
+    return f'You said "{noun}".'
+
+
+def poke(noun):
+    return f'You poked {noun}!'
 
 
 verb_dict = {
-    "say": say
+    'say': say,
+    'poke': poke
 }
 
-while True:
-    get_input()
+if __name__ == '__main__':
+    while True:
+        run()
